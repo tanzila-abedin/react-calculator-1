22d987065b503a1dd61cb90def8fc98379a25952
Ryel Banfield <ryelbanfield@gmail.com>
2021-07-02 17:02:01 -0400

    Replace div with react fragment

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import ButtonPanel from './ButtonPanel';
 const result = '0';
 
 const App = () => (
-  <div className="App">
+  <>
     <header className="App-header">
       Calculator built using React
     </header>
     <Display result={result} />
     <ButtonPanel />
-  </div>
+  </>
 );
 
 export default App;
