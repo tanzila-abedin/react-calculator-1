ed048a08abf1707b356467c7e878c057e33a2bdb
Ryel Banfield <ryelbanfield@gmail.com>
2021-07-13 10:33:26 -0400

    Remove tests

--- src/__tests__/Quotes.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const quote = renderer
-    .create(<div><h2>Everything you can imagine is real. – Pablo Picasso</h2></div>)
-    .toJSON();
-  expect(quote).toMatchSnapshot();
-});
